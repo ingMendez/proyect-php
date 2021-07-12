fab4654ffd3df8f285ed5bdc2968d1fb91f9ead5
Reydi alejandro Mendez Suarez <42841326+ingMendez@users.noreply.github.com>
2021-07-12 11:04:48 -0400

    Add files via upload

---
+++ aprenderPHP/sintexis.php
@@ -0,0 +1,8 @@
+<?php
+
+print "hola mundo";
+echo "<h1>
+hola</h1>";
+
+
+?>
---
+++ aprenderPHP/variables.php
@@ -0,0 +1,13 @@
+<?php
+
+#Esta son decaraciones de variables en php. para la declaraciones de las variables se utiliza el singno $  .
+$numero = 5;
+print "el numero es : $numero";
+echo "<br><br>";
+
+$palabra = "palabra";
+echo "esta es una variable de texto : $palabra";
+
+
+
+?>
